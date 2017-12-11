0a8fc678f29f7a48d0d7fe02629181fa8f215dab
kaiyu5609 <1047935422@qq.com>
2017-12-11 21:35:49 +0800

    RowDataSet
    RowDataSet

--- public/dataset/RowDataSet.js
+++ public/dataset/RowDataSet.js
@@ -10,67 +10,67 @@ define(function(require, exports, module) {
 
         find(bizParameter) {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <find> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         sort(sorts) {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <sort> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         filter(filters) {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <filter> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         count() {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <count> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         sum(fieldName) {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <sum> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         max(fieldName) {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <max> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         min(fieldName) {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <min> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         operate() {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <operate> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         orderBy() {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <orderBy> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         catch() {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <catch> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
         saveAs(parameters, format) {
             // <debug>
-            console.warn("The Method <> subclass that you are using definde a function to extend.");
+            console.warn("The Method <saveAs> subclass that you are using definde a function to extend.");
             // </debug>
         }
 
